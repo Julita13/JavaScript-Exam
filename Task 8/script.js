7720e054cc5bdeaa6867394ada4772e34cd346c2
Julita13 <julitamazuolyte@yahoo.com>
2022-12-07 22:23:37 +0200

    8th task completed

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,29 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+// ES5
+
+function Calculator(number1, number2){
+    this.num1 = number1;
+    this.num2 = number2;
+    this.sum = function() {
+        console.log(this.num1 + this.num2);
+    };
+    this.subtraction = function() {
+        console.log(this.num1 - this.num2)
+    };
+    this.multiplication = function() {
+        console.log(this.num1 * this.num2)
+    };
+    this.division = function() {
+        console.log(this.num1 / this.num2)
+    }
+}
+
+const kalkuliatorius = new Calculator(20, 5);
+kalkuliatorius.sum();
+kalkuliatorius.subtraction();
+kalkuliatorius.multiplication();
+kalkuliatorius.division();
+
